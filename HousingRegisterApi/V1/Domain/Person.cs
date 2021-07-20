a1b106b15655a363ef0d506e0df0c48160882861
Thomas Morris <thomas.morris@manifesto.co.uk>
2021-07-20 17:15:00 +0100

    Update person

--- HousingRegisterApi/V1/Domain/Person.cs
+++ HousingRegisterApi/V1/Domain/Person.cs
@@ -5,15 +5,25 @@ namespace HousingRegisterApi.V1.Domain
     // TODO: integrate with the Person API
     public class Person
     {
+        public Person()
+        {
+            if (Id == Guid.Empty)
+            {
+                Id = Guid.NewGuid();
+            }
+        }
+
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public string Firstname { get; set; }
+        public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string Surname { get; set; }
-        public string Ethnicity { get; set; }
-        public string Nationality { get; set; }
         public string PlaceOfBirth { get; set; }
         public DateTime DateOfBirth { get; set; }
+
+        // these are no longer present in Person API
+        public string Ethnicity { get; set; }
+        public string Nationality { get; set; }
         public string Gender { get; set; }
     }
 }
