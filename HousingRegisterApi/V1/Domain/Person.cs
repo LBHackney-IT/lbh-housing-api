54ec532eeb026e995aab4114948dce3187e4ce4d
Thomas Morris <thomas.morris@manifesto.co.uk>
2021-09-14 12:17:28 +0100

    Remove unused props from person

--- HousingRegisterApi/V1/Domain/Person.cs
+++ HousingRegisterApi/V1/Domain/Person.cs
@@ -18,19 +18,12 @@ namespace HousingRegisterApi.V1.Domain
         public string Title { get; set; }
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
-
         public string Surname { get; set; }
 
-        public string PlaceOfBirth { get; set; }
         public DateTime DateOfBirth { get; set; }
-
-        public string PersonType { get; set; }
-        public string RelationshipType { get; set; }
-
-        public string Ethnicity { get; set; }
-        public string Nationality { get; set; }
         public string Gender { get; set; }
         public string GenderDescription { get; set; }
+        public string RelationshipType { get; set; }
 
         public string NationalInsuranceNumber { get; set; }
     }
