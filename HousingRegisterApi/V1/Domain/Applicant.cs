fd9616372c2b3493af48dfde8cf064391e09082f
Tony Gledhill <anthony.gledhill@manifesto.co.uk>
2021-09-27 19:00:31 +0100

    Created medical classes

--- HousingRegisterApi/V1/Domain/Applicant.cs
+++ HousingRegisterApi/V1/Domain/Applicant.cs
@@ -5,12 +5,10 @@ namespace HousingRegisterApi.V1.Domain
     public class Applicant
     {
         public Person Person { get; set; }
-
         public Address Address { get; set; }
         public ContactInformation ContactInformation { get; set; }
-
         public IEnumerable<Question> Questions { get; set; }
-
-        // TODO: other connecting information for the application, e.g. evidence?
+        public MedicalNeed MedicalNeed { get; set; }
+        public MedicalOutcome MedicalOutcome { get; set; }
     }
 }
---
+++ HousingRegisterApi/V1/Domain/MedicalNeed.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace HousingRegisterApi.V1.Domain
+{
+    public class MedicalNeed
+    {
+        public DateTime FormRecieved { get; set; }
+        public string FormLink { get; set; }
+    }
+}
