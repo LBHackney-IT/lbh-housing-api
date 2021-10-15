9098806347c878c25354ee761a2514e00f7e8e96
Thomas Morris <thomas.morris@manifesto.co.uk>
2021-10-15 11:54:19 +0100

    Update handling of medical needs

--- HousingRegisterApi/V1/Domain/MedicalNeed.cs
+++ HousingRegisterApi/V1/Domain/MedicalNeed.cs
@@ -5,6 +5,12 @@ namespace HousingRegisterApi.V1.Domain
     public class MedicalNeed
     {
         public DateTime FormRecieved { get; set; }
-        public string FormLink { get; set; }
+        public DateTime? AssessmentDate { get; set; }
+
+        public string Outcome { get; set; }
+        public string AccessibileHousingRegister { get; set; }
+        public string Disability { get; set; }
+
+        public string AdditionalInformaton { get; set; }
     }
 }
--- HousingRegisterApi/V1/Domain/Question.cs
+++ HousingRegisterApi/V1/Domain/Question.cs
@@ -1,11 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
 namespace HousingRegisterApi.V1.Domain
 {
     public class Question
     {
-        // eg. householdincomeaboveX
+        // eg. section-name/question
         public string Id { get; set; }
 
         // eg. yes/no
         public string Answer { get; set; }
+    }
+
+    public static class QuestionExtensions
+    {
+        public static string GetAnswer(this IEnumerable<Question> questions, string name)
+        {
+            var question = questions?.FirstOrDefault(x => x.Id == name);
+            return JsonSerializer.Deserialize<string>(question?.Answer) ?? string.Empty;
+        }
     }
 }
