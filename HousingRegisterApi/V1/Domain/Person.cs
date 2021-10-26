d9a66ed2ff5d08ef2f581507c4bd93be6ced3b7d
jas attilia <jasvir@btinternet.com>
2021-10-26 18:38:05 +0100

    copied bedroom logic and tests from web portal

--- HousingRegisterApi/V1/Domain/Person.cs
+++ HousingRegisterApi/V1/Domain/Person.cs
@@ -14,18 +14,16 @@ namespace HousingRegisterApi.V1.Domain
         }
 
         public Guid Id { get; set; }
-
         public string Title { get; set; }
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string Surname { get; set; }
         public string FullName => FirstName + " " + Surname;
-
         public DateTime DateOfBirth { get; set; }
         public string Gender { get; set; }
         public string GenderDescription { get; set; }
         public string RelationshipType { get; set; }
-
         public string NationalInsuranceNumber { get; set; }
+        public int Age => DateTime.UtcNow.Year - DateOfBirth.Year;
     }
 }
