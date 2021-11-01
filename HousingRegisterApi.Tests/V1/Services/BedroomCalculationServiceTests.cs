4b5f76f3459d00e14ee032eae280b8a6586595df
jas attilia <jasvir@btinternet.com>
2021-11-01 09:11:32 +0000

    task:HRT253 - added original tests back in

--- HousingRegisterApi.Tests/V1/Services/BedroomCalculationServiceTests.cs
+++ HousingRegisterApi.Tests/V1/Services/BedroomCalculationServiceTests.cs
@@ -13,140 +13,140 @@ namespace HousingRegisterApi.Tests.V1.Services
         private const string Male = "male";
         private const string Female = "female";
 
-        //[Test(Description = "Should award 1 bedroom for different genders under the age of 10")]
-        //public void ApplicationShouldAward1BedroomForDifferentGendersUnderTheAgeOf10()
-        //{
-        //    // arrange
-        //    bool partnerSharing = false;
-        //    Application application = CreateApplication(new List<Tuple<int, string>>()
-        //    {
-        //        new Tuple<int, string>(5, Male),
-        //        new Tuple<int, string>(5, Female)
-        //    }, partnerSharing);
+        [Test(Description = "Should award 1 bedroom for different genders under the age of 10")]
+        public void ApplicationShouldAward1BedroomForDifferentGendersUnderTheAgeOf10()
+        {
+            // arrange
+            bool partnerSharing = false;
+            Application application = CreateApplication(new List<Tuple<int, string>>()
+            {
+                new Tuple<int, string>(5, Male),
+                new Tuple<int, string>(5, Female)
+            }, partnerSharing);
 
-        //    // act
-        //    var expected = 1;
-        //    var actual = BedroomCalculationService.Calculate(application);
+            // act
+            var expected = 1;
+            var actual = BedroomCalculationService.Calculate(application);
 
-        //    // assert
-        //    Assert.AreEqual(expected, actual);
-        //}
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
 
-        //[Test(Description = "Children of different genders where one is over 10 should be awarded 2 bedrooms")]
-        //public void ApplicationWithChildrenOfDifferentGendersWhereOneIsOver10ShouldBeAwarded2Bedrooms()
-        //{
-        //    // arrange
-        //    bool partnerSharing = false;
-        //    Application application = CreateApplication(new List<Tuple<int, string>>()
-        //    {
-        //        new Tuple<int, string>(5, Male),
-        //        new Tuple<int, string>(10, Female)
-        //    }, partnerSharing);
+        [Test(Description = "Children of different genders where one is over 10 should be awarded 2 bedrooms")]
+        public void ApplicationWithChildrenOfDifferentGendersWhereOneIsOver10ShouldBeAwarded2Bedrooms()
+        {
+            // arrange
+            bool partnerSharing = false;
+            Application application = CreateApplication(new List<Tuple<int, string>>()
+            {
+                new Tuple<int, string>(5, Male),
+                new Tuple<int, string>(10, Female)
+            }, partnerSharing);
 
-        //    // act
-        //    var expected = 2;
-        //    var actual = BedroomCalculationService.Calculate(application);
+            // act
+            var expected = 2;
+            var actual = BedroomCalculationService.Calculate(application);
 
-        //    // assert
-        //    Assert.AreEqual(expected, actual);
-        //}
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
 
-        //[Test(Description = "Children of ages between 5 to 15 of different genders should be awarded 2 bedrooms")]
-        //public void ApplicationWithChildrenOfAgesBetween5To15OfDifferentGendersShouldBeAwarded2Bedrooms()
-        //{
-        //    // arrange
-        //    bool partnerSharing = false;
-        //    Application application = CreateApplication(new List<Tuple<int, string>>()
-        //    {
-        //        new Tuple<int, string>(5, Male),
-        //        new Tuple<int, string>(15, Male),
-        //        new Tuple<int, string>(10, Female)
-        //    }, partnerSharing);
+        [Test(Description = "Children of ages between 5 to 15 of different genders should be awarded 2 bedrooms")]
+        public void ApplicationWithChildrenOfAgesBetween5To15OfDifferentGendersShouldBeAwarded2Bedrooms()
+        {
+            // arrange
+            bool partnerSharing = false;
+            Application application = CreateApplication(new List<Tuple<int, string>>()
+            {
+                new Tuple<int, string>(5, Male),
+                new Tuple<int, string>(15, Male),
+                new Tuple<int, string>(10, Female)
+            }, partnerSharing);
 
-        //    // act
-        //    var expected = 2;
-        //    var actual = BedroomCalculationService.Calculate(application);
+            // act
+            var expected = 2;
+            var actual = BedroomCalculationService.Calculate(application);
 
-        //    // assert
-        //    Assert.AreEqual(expected, actual);
-        //}
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
 
-        //[Test(Description = "2 boys aged between 5 and 15 and 2 girls aged 10 and 12 should be awarded 2 bedrooms")]
-        //public void ApplicationWith2BoysAgedBetween5And15And2GirlsAged10And12ShouldBeAwarded2Bedrooms()
-        //{
-        //    // arrange
-        //    bool partnerSharing = false;
-        //    Application application = CreateApplication(new List<Tuple<int, string>>()
-        //    {
-        //        new Tuple<int, string>(5, Male),
-        //        new Tuple<int, string>(15, Male),
-        //        new Tuple<int, string>(10, Female),
-        //        new Tuple<int, string>(12, Female)
-        //    }, partnerSharing);
+        [Test(Description = "2 boys aged between 5 and 15 and 2 girls aged 10 and 12 should be awarded 2 bedrooms")]
+        public void ApplicationWith2BoysAgedBetween5And15And2GirlsAged10And12ShouldBeAwarded2Bedrooms()
+        {
+            // arrange
+            bool partnerSharing = false;
+            Application application = CreateApplication(new List<Tuple<int, string>>()
+            {
+                new Tuple<int, string>(5, Male),
+                new Tuple<int, string>(15, Male),
+                new Tuple<int, string>(10, Female),
+                new Tuple<int, string>(12, Female)
+            }, partnerSharing);
 
-        //    // act
-        //    var expected = 2;
-        //    var actual = BedroomCalculationService.Calculate(application);
+            // act
+            var expected = 2;
+            var actual = BedroomCalculationService.Calculate(application);
 
-        //    // assert
-        //    Assert.AreEqual(expected, actual);
-        //}
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
 
-        //[Test(Description = "should calculate correct amount of bedrooms for couple without children")]
-        //public void ApplicationShouldCalculateCorrectAmountOfBedroomsForCoupleWithoutChildren()
-        //{
-        //    // arrange
-        //    bool partnerSharing = true;
-        //    Application application = CreateApplication(new List<Tuple<int, string>>()
-        //    {
-        //        new Tuple<int, string>(30, Male),
-        //        new Tuple<int, string>(25, Female)
-        //    }, partnerSharing);
+        [Test(Description = "should calculate correct amount of bedrooms for couple without children")]
+        public void ApplicationShouldCalculateCorrectAmountOfBedroomsForCoupleWithoutChildren()
+        {
+            // arrange
+            bool partnerSharing = true;
+            Application application = CreateApplication(new List<Tuple<int, string>>()
+            {
+                new Tuple<int, string>(30, Male),
+                new Tuple<int, string>(25, Female)
+            }, partnerSharing);
 
-        //    // act
-        //    var expected = 1;
-        //    var actual = BedroomCalculationService.Calculate(application);
+            // act
+            var expected = 1;
+            var actual = BedroomCalculationService.Calculate(application);
 
-        //    // assert
-        //    Assert.AreEqual(expected, actual);
-        //}
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
 
-        //[Test(Description = "should calculate 2 bedrooms for couple with children under the age of 10(different gender)")]
-        //public void ApplicationShouldCalculate2BedroomsForCoupleWithChildrenUnderTheAgeOf10WithDifferentGender()
-        //{
-        //    // arrange
-        //    bool partnerSharing = true;
-        //    Application application = CreateApplication(new List<Tuple<int, string>>()
-        //    {
-        //        new Tuple<int, string>(30, Male),
-        //        new Tuple<int, string>(25, Female),
-        //        new Tuple<int, string>(5, Male),
-        //        new Tuple<int, string>(7, Female)
-        //    }, partnerSharing);
+        [Test(Description = "should calculate 2 bedrooms for couple with children under the age of 10(different gender)")]
+        public void ApplicationShouldCalculate2BedroomsForCoupleWithChildrenUnderTheAgeOf10WithDifferentGender()
+        {
+            // arrange
+            bool partnerSharing = true;
+            Application application = CreateApplication(new List<Tuple<int, string>>()
+            {
+                new Tuple<int, string>(30, Male),
+                new Tuple<int, string>(25, Female),
+                new Tuple<int, string>(5, Male),
+                new Tuple<int, string>(7, Female)
+            }, partnerSharing);
 
-        //    // act
-        //    var expected = 2;
-        //    var actual = BedroomCalculationService.Calculate(application);
+            // act
+            var expected = 2;
+            var actual = BedroomCalculationService.Calculate(application);
 
-        //    // assert
-        //    Assert.AreEqual(expected, actual);
-        //}
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
 
-        //[Test(Description = "should calculate 3 bedrooms for couple with children over the age of 10(different gender)")]
-        //public void ApplicationShouldCalculate3BedroomsForCoupleWithChildrenOverTheAgeOf10WithDifferentGender()
-        //{
-        //    // arrange
-        //    bool partnerSharing = true;
-        //    Application application = CreateApplication(new List<Tuple<int, string>>()
-        //    {
-        //        new Tuple<int, string>(30, Male),
-        //        new Tuple<int, string>(25, Female),
-        //        new Tuple<int, string>(11, Male),
-        //        new Tuple<int, string>(12, Female)
-        //    }, partnerSharing);
+        [Test(Description = "should calculate 3 bedrooms for couple with children over the age of 10(different gender)")]
+        public void ApplicationShouldCalculate3BedroomsForCoupleWithChildrenOverTheAgeOf10WithDifferentGender()
+        {
+            // arrange
+            bool partnerSharing = true;
+            Application application = CreateApplication(new List<Tuple<int, string>>()
+            {
+                new Tuple<int, string>(30, Male),
+                new Tuple<int, string>(25, Female),
+                new Tuple<int, string>(11, Male),
+                new Tuple<int, string>(12, Female)
+            }, partnerSharing);
 
-        //    AssertBedrooms(3, application);
-        //}
+            AssertBedrooms(3, application);
+        }
 
         [Test(Description = "Single person under 35yrs returns 0 bedrooms")]
         public void SinglePersonUnder35yrsReturns0Bedrooms()
