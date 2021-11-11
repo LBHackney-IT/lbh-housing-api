df2c60538b827aa15816e3e6a3d5675bf6b25608
Dan James <danj@deeson.co.uk>
2021-11-11 10:42:13 +0000

    Trying out adding an additional Lambda entry point.

---
+++ HousingRegisterApi/RedroomRecalculator.cs
@@ -0,0 +1,13 @@
+using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HousingRegisterApi
+{
+    public class BedroomRecalculator
+    {
+        protected override void Recalculate()
+        {
+            throw new Exception('this is happening')
+        }
+    }
+}
