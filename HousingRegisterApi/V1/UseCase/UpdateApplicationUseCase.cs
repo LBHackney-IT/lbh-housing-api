b0d9048cc2f562668f9c24530f1087db536424bf
jas attilia <jasvir@btinternet.com>
2021-11-15 16:09:04 +0000

    feature:hrt159: fixed missing namespace issue

--- HousingRegisterApi/V1/UseCase/UpdateApplicationUseCase.cs
+++ HousingRegisterApi/V1/UseCase/UpdateApplicationUseCase.cs
@@ -1,12 +1,11 @@
-using System;
 using HousingRegisterApi.V1.Boundary.Request;
 using HousingRegisterApi.V1.Boundary.Response;
 using HousingRegisterApi.V1.Boundary.Response.Exceptions;
 using HousingRegisterApi.V1.Factories;
 using HousingRegisterApi.V1.Gateways;
-using HousingRegisterApi.V1.Gateways.Interfaces;
 using HousingRegisterApi.V1.Infrastructure;
 using HousingRegisterApi.V1.UseCase.Interfaces;
+using System;
 
 namespace HousingRegisterApi.V1.UseCase
 {
