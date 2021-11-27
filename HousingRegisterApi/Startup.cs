c4d26b3aa6fafe9c6a2db3d0150d4d73b7ee493f
jas attilia <jasvir@btinternet.com>
2021-11-27 13:34:32 +0000

    bug:hrt382 - fixied formatting

--- HousingRegisterApi/Startup.cs
+++ HousingRegisterApi/Startup.cs
@@ -171,7 +171,7 @@ namespace HousingRegisterApi
             services.AddScoped<ICreateNovaletExportUseCase, CreateNovaletExportUseCase>();
             services.AddScoped<IGetNovaletExportUseCase, GetNovaletExportUseCase>();
             services.AddScoped<IListNovaletExportFilesUseCase, ListNovaletExportFilesUseCase>();
-            services.AddScoped<IApproveNovaletExportUseCase, ApproveNovaletExportUseCase>();            
+            services.AddScoped<IApproveNovaletExportUseCase, ApproveNovaletExportUseCase>();
             services.AddScoped<IViewingApplicationUseCase, ViewingApplicationUseCase>();
 
             services.AddScoped<ISHA256Helper, SHA256Helper>();
