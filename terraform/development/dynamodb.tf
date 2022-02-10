fbe57efba7ce5394132ed78d65476eb332fbc517
Dan James <danj@deeson.co.uk>
2022-02-10 11:49:46 +0000

    another test

--- terraform/development/dynamodb.tf
+++ terraform/development/dynamodb.tf
@@ -5,17 +5,6 @@ resource "aws_dynamodb_table" "housingregisterapi_dynamodb_table" {
     write_capacity        = 10
     hash_key              = "id"
 
-    autoscaling_enabled = true
-    autoscaling_read = {
-        target_value       = 70
-        max_capacity       = 40
-    }
-
-    autoscaling_write = {
-        target_value       = 70
-        max_capacity       = 40
-    }
-
     attribute {
         name              = "id"
         type              = "S"
@@ -27,4 +16,28 @@ resource "aws_dynamodb_table" "housingregisterapi_dynamodb_table" {
         terraform-managed = true
         project_name      = var.project_name
     }
+}
+
+resource "aws_appautoscaling_target" "housingregisterapi_dynamodb_table_read_target" {
+  max_capacity       = 100
+  min_capacity       = 5
+  resource_id        = "table/tableName"
+  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "housingregisterapi_dynamodb_table_read_policy" {
+  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.housingregisterapi_dynamodb_table_read_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.housingregisterapi_dynamodb_table_read_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.housingregisterapi_dynamodb_table_read_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.housingregisterapi_dynamodb_table_read_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+
+    target_value = 70
+  }
 }
