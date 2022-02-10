302334a751b4c5d078ff7ce4816c1312379dddd4
Dan James <danj@deeson.co.uk>
2022-02-10 14:06:25 +0000

    Increase write capacity too

--- terraform/staging/dynamodb.tf
+++ terraform/staging/dynamodb.tf
@@ -41,3 +41,27 @@ resource "aws_appautoscaling_policy" "housingregisterapi_dynamodb_table_read_pol
     target_value = 70
   }
 }
+
+resource "aws_appautoscaling_target" "housingregisterapi_dynamodb_table_write_target" {
+  max_capacity       = 50
+  min_capacity       = 5
+  resource_id        = "table/HousingRegister"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "housingregisterapi_dynamodb_table_write_policy" {
+  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.housingregisterapi_dynamodb_table_write_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.housingregisterapi_dynamodb_table_write_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.housingregisterapi_dynamodb_table_write_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.housingregisterapi_dynamodb_table_write_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+
+    target_value = 70
+  }
+}
