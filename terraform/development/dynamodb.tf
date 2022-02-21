dc63ffbec0838c5448604b0075931ef6f66c6a92
Dan James <danj@deeson.co.uk>
2022-02-21 22:02:42 +0000

    Think I've got it this time.

--- terraform/development/dynamodb.tf
+++ terraform/development/dynamodb.tf
@@ -2,8 +2,7 @@ locals {
   defaultCapacity = 10
   minCapacity = 2
   maxCapacity = 50
-  resourceNames = toset([
-    "table/HousingRegister",
+  indexNames = toset([
     "table/HousingRegister/index/HousingRegisterAll",
     "table/HousingRegister/index/HousingRegisterStatus",
     "table/HousingRegister/index/HousingRegisterAssignedTo",
@@ -91,19 +90,17 @@ resource "aws_dynamodb_table" "housingregisterapi_dynamodb_table" {
 }
 
 resource "aws_appautoscaling_target" "housingregisterapi_dynamodb_table_read_target" {
-  for_each = local.resourceNames
   max_capacity       = local.maxCapacity
   min_capacity       = local.minCapacity
-  resource_id        = each.key
+  resource_id        = "table/HousingRegister"
   scalable_dimension = "dynamodb:table:ReadCapacityUnits"
   service_namespace  = "dynamodb"
 }
 
 resource "aws_appautoscaling_policy" "housingregisterapi_dynamodb_table_read_policy" {
-  for_each = local.resourceNames
-  name               = "DynamoDBReadCapacityUtilization:${each.key}"
+  name               = "DynamoDBReadCapacityUtilization:table/HousingRegister"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = each.key
+  resource_id        = "table/HousingRegister"
   scalable_dimension = "dynamodb:table:ReadCapacityUnits"
   service_namespace  = "dynamodb"
 
@@ -117,20 +114,70 @@ resource "aws_appautoscaling_policy" "housingregisterapi_dynamodb_table_read_pol
 }
 
 resource "aws_appautoscaling_target" "housingregisterapi_dynamodb_table_write_target" {
-  for_each = local.resourceNames
   max_capacity       = local.maxCapacity
   min_capacity       = local.minCapacity
-  resource_id        = each.key
+  resource_id        = "table/HousingRegister"
   scalable_dimension = "dynamodb:table:WriteCapacityUnits"
   service_namespace  = "dynamodb"
 }
 
 resource "aws_appautoscaling_policy" "housingregisterapi_dynamodb_table_write_policy" {
-  for_each = local.resourceNames
+  name               = "DynamoDBWriteCapacityUtilization:table/HousingRegister"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = "table/HousingRegister"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+
+    target_value = 70
+  }
+}
+
+resource "aws_appautoscaling_target" "housingregisterapi_dynamodb_index_read_target" {
+  for_each = local.indexNames
+  max_capacity       = local.maxCapacity
+  min_capacity       = local.minCapacity
+  resource_id        = each.key
+  scalable_dimension = "dynamodb:index:ReadCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "housingregisterapi_dynamodb_index_read_policy" {
+  for_each = local.indexNames
+  name               = "DynamoDBReadCapacityUtilization:${each.key}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = each.key
+  scalable_dimension = "dynamodb:index:ReadCapacityUnits"
+  service_namespace  = "dynamodb"
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+
+    target_value = 70
+  }
+}
+
+resource "aws_appautoscaling_target" "housingregisterapi_dynamodb_index_write_target" {
+  for_each = local.indexNames
+  max_capacity       = local.maxCapacity
+  min_capacity       = local.minCapacity
+  resource_id        = each.key
+  scalable_dimension = "dynamodb:index:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "housingregisterapi_dynamodb_index_write_policy" {
+  for_each = local.indexNames
   name               = "DynamoDBWriteCapacityUtilization:${each.key}"
   policy_type        = "TargetTrackingScaling"
   resource_id        = each.key
-  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  scalable_dimension = "dynamodb:index:WriteCapacityUnits"
   service_namespace  = "dynamodb"
 
   target_tracking_scaling_policy_configuration {
