b8b82b29afe9f3722b03bb10fb00cce870761050
Huss <57395585+anefiox@users.noreply.github.com>
2022-05-05 13:23:38 +0100

    Add notes in any state

--- HousingRegisterApi/V1/Gateways/ApplicationActivityGateway.cs
+++ HousingRegisterApi/V1/Gateways/ApplicationActivityGateway.cs
@@ -51,9 +51,7 @@ namespace HousingRegisterApi.V1.Gateways
         {
             // we only want to log activites after an application has been submitted
             if (activity == null
-                || application == null
-                || application.Status == ApplicationStatus.Verification
-                || application.Status == ApplicationStatus.New)
+                || application == null)
             {
                 return;
             }
