6bd80462670233526de22d7a2b9b180608fabd0e
Huss <57395585+anefiox@users.noreply.github.com>
2022-07-04 12:17:23 +0100

    Just check for Person Firstname

--- HousingRegisterApi/V1/UseCase/UpdateApplicationUseCase.cs
+++ HousingRegisterApi/V1/UseCase/UpdateApplicationUseCase.cs
@@ -131,10 +131,10 @@ namespace HousingRegisterApi.V1.UseCase
                         "Assessment.BiddingNumber", application.Assessment?.BiddingNumber, request.Assessment.BiddingNumber));
                 }
 
-                if (request.MainApplicant != null)
+                if (request.MainApplicant?.Person?.FirstName != null)
                 {
-                    activities.Add(new EntityActivity<ApplicationActivityType>(ApplicationActivityType.MainApplicantChangedByUser,
-                        "MainApplicant", application.MainApplicant, request.MainApplicant));
+                    activities.Add(new EntityActivity<ApplicationActivityType>(ApplicationActivityType.PersonChangedByUser,
+                        "MainApplicant.Person.FirstName", application.MainApplicant?.Person?.FirstName, request.MainApplicant?.Person?.FirstName));
                 }
 
                 //Remove all of the above and check the application against the request.
