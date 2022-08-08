42647d89bc43a561a8c7e5d9b443514d5871b4c1
Matt Dendle <matt.dendle@foundry4.com>
2022-08-08 12:42:27 +0100

    cleaned usings

--- HousingRegisterApi/V1/UseCase/GetAllApplicationsUseCase.cs
+++ HousingRegisterApi/V1/UseCase/GetAllApplicationsUseCase.cs
@@ -1,12 +1,9 @@
 using HousingRegisterApi.V1.Boundary.Request;
 using HousingRegisterApi.V1.Boundary.Response;
-using HousingRegisterApi.V1.Gateways;
-using HousingRegisterApi.V1.Gateways.Interfaces;
-using HousingRegisterApi.V1.Infrastructure;
-using HousingRegisterApi.V1.UseCase.Interfaces;
-using System.Linq;
-using System.Threading.Tasks;
 using HousingRegisterApi.V1.Factories;
+using HousingRegisterApi.V1.Gateways.Interfaces;
+using HousingRegisterApi.V1.UseCase.Interfaces;
+using System.Threading.Tasks;
 
 namespace HousingRegisterApi.V1.UseCase
 {
--- HousingRegisterApi/V1/UseCase/GetApplicationsByReferenceUseCase.cs
+++ HousingRegisterApi/V1/UseCase/GetApplicationsByReferenceUseCase.cs
@@ -1,9 +1,9 @@
 using HousingRegisterApi.V1.Boundary.Request;
 using HousingRegisterApi.V1.Boundary.Response;
+using HousingRegisterApi.V1.Factories;
 using HousingRegisterApi.V1.Gateways.Interfaces;
 using HousingRegisterApi.V1.UseCase.Interfaces;
 using System.Threading.Tasks;
-using HousingRegisterApi.V1.Factories;
 
 namespace HousingRegisterApi.V1.UseCase
 {
