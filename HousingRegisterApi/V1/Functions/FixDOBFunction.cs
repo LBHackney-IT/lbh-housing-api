ea68590f1c0489634702d91d3d171e049029daf9
Matt Dendle <matt.dendle@foundry4.com>
2022-08-26 15:33:30 +0100

    wrapped in function

---
+++ HousingRegisterApi/V1/Functions/FixDOBFunction.cs
@@ -0,0 +1,29 @@
+using Amazon.Lambda.Core;
+using HousingRegisterApi.V1.Infrastructure;
+using HousingRegisterApi.V1.UseCase;
+using HousingRegisterApi.V1.UseCase.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace HousingRegisterApi.V1.Functions
+{
+    public class FixDOBFunction : BaseFunction
+    {
+        protected async Task Handle(ILambdaContext context)
+        {
+            context.Logger.LogLine("Fixing DOBs");
+            var useCase = ServiceProvider.GetService<IFixDOBUseCase>();
+            await useCase.Execute().ConfigureAwait(false);
+        }
+
+        protected override void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureDynamoDB();
+            services.AddHttpClient();
+
+            services.AddScoped<IFixDOBUseCase, FixDOBUseCase>();
+
+            base.ConfigureServices(services);
+        }
+    }
+}
