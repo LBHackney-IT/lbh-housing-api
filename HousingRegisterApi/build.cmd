953cee1f8c87a3f6f2b274cf962ef90d3edfe0cb
Marta Pederiva <86296201+martapederiva@users.noreply.github.com>
2025-01-28 11:51:19 +0000

    .net upgrades

--- HousingRegisterApi/build.cmd
+++ HousingRegisterApi/build.cmd
@@ -1,2 +1,2 @@
 dotnet restore
-dotnet lambda package --configuration release --framework net8.0 --output-package bin/release/net/housing-register-api.zip
+dotnet lambda package --configuration release --framework net8.0 --output-package bin/release/net8.0/housing-register-api.zip
